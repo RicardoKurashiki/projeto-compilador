87b0afe68fc2616142ffc2add85ab046cd6bcb52
Ricardo Godoi Kurashiki <62195354+RicardoKurashiki@users.noreply.github.com>
2023-06-25 18:56:07 -0300

    Finalização do trabalho

--- Fase 3/main.py
+++ Fase 3/main.py
@@ -11,11 +11,22 @@ def getFileName():
     return sys.argv[1]
 
 if __name__ == "__main__":
+    print("== INICIO ==")
     fileName = getFileName()
     device = Arduino()
+    print(f"Rodando arquivo: files/{fileName}.txt")
     script = File(f"files/{fileName}.txt")              # Arquivo de Input
+    print(f"Salvando em: output/{fileName}_output.asm")
     output = File(f"output/{fileName}_output.asm")      # Arquivo de Output
+    print()
+    print("Separando instruções...")
     interpreter = tokens.run(script)
+    print("Instruções separadas!")
+    print("Traduzindo para asm...")
     result = interpreter.run(device)
+    print("Tradução realizada!")
     output.writeFile("")
+    print("Escrevendo no arquivo de output...")
     output.writeFile(result)
+    print("Escrita feita!")
+    print("== FIM ==")
